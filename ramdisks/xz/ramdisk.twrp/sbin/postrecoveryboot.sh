16642b39cdb9f7dc1cd4d116237341482df30f4a
xNUTx <nut@fun-industries.nl>
2015-06-22 12:34:23 +0200

    Cleanup of the old fix, which only partially fixed the issue.

--- ramdisks/xz/ramdisk.twrp/sbin/postrecoveryboot.sh
+++ ramdisks/xz/ramdisk.twrp/sbin/postrecoveryboot.sh
@@ -5,21 +5,4 @@ if [ "$(mount | grep '/storage/sdcard1' | wc -l)" = "1" ]; then
 	return $?
 fi
 
-SYSTEM=$(/sbin/busybox find /dev/block/platform/msm_sdcc.1/by-name/ -iname "system")
-
-# Thanks to McBane87 @ XDA for the initial fix which is the base of this snippet of code.
-/sbin/umount /system
-/sbin/busybox blockdev --setrw $SYSTEM
-
-/sbin/e2fsck -pcf $SYSTEM
-if [ "$?" -ne "0" ]; then
-
-	return $?
-
-fi
-
-/sbin/mount -o ro /system
-/sbin/mount -o remount,rw /system
-/sbin/umount /system
-
 return 0
